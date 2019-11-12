291402f3906416c4010bd700867827c6a919750b
Tanya Fonina <tanya.fonina@envato.com>
2019-11-13 00:22:27 +1100

    Make rubocop happy

--- lib/query_parser.rb
+++ lib/query_parser.rb
@@ -3,7 +3,13 @@
 class QueryParser
   BadData = Class.new(StandardError)
 
-  REGEX = /^\s*select\s+(?<collection>\w+)\s+where\s+(?<attribute>\w+)\s+(?<operator>[=\>\<\-\+\w])\s+(?<value>.*)$/i.freeze
+  REGEX = /^\s*
+    select
+    \s+(?<collection>\w+)
+    \s+where
+    \s+(?<attribute>\w+)
+    \s+(?<operator>[=\>\<\-\+\w])
+    \s+(?<value>.*)/ix.freeze
 
   def self.parse(query_string)
     match = REGEX.match(query_string)
